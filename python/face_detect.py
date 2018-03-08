9264ff57590e1e23241af0745ed8c86cc38db1b9
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2018-03-08 15:37:11 +0000

    Added python example for face detection

---
+++ python/face_detect.py
@@ -0,0 +1,27 @@
+import cv2
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Faces', frame)
+    cv2.waitKey(10)
+
